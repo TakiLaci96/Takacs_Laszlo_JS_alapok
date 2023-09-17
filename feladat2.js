66e7eae876b44de5f60dd7d1dd818fabe077bce1
Takács László <takilaci96@gmail.com>
2023-09-17 17:36:33 +0200

    masodik feladat

---
+++ feladat2.js
@@ -0,0 +1,7 @@
+function bevitelZoldre(input) {
+    document.getElementById("mezo1").style.color = 'black';
+    document.getElementById("mezo2").style.color = 'black';
+    document.getElementById("mezo3").style.color = 'black';
+
+    input.style.color = 'green';
+}
